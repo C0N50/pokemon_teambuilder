c5097e1385c68d6873891c147f715d92b0daf072
Connor Ferris <ferr0373@gmail.com>
2023-03-13 21:16:18 -0500

    added pokeball side backgrounds css

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
+import './LoginPage.css'
+import Button from '@mui/material/Button';
 
 function LoginPage() {
   const history = useHistory();
 
   return (
-    <div>
+    <div className = 'bg'>
+
+      <div>
       <LoginForm />
+      </div>
 
       <center>
-        <button
+        <Button
+        variant='contained'
           type="button"
           className="btn btn_asLink"
           onClick={() => {
@@ -18,9 +24,10 @@ function LoginPage() {
           }}
         >
           Register
-        </button>
+        </Button>
       </center>
     </div>
+    
   );
 }
 
