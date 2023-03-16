6ee16278ae98438a2e05b74f0a02a414aeec29ae
Connor Ferris <ferr0373@gmail.com>
2023-03-16 10:27:26 -0500

    added edit page with navigation -edit- buttons to and from the page on the seletected team pokemon component

--- src/components/PokemonEditPage/PokemonEditPage.jsx
+++ src/components/PokemonEditPage/PokemonEditPage.jsx
@@ -4,14 +4,30 @@ import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import AllPokemonList from '../AllPokemonList/AllPokemonList';
+import SelectedTeam from '../SelectedTeam/SelectedTeam';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
 
-function PokemonEditPage () {
+function PokemonEditPage ({handlePokemonEditClick, team}) {
+
+
+    const saveChanges = () => {
+        console.log('TO ADD: SAVING THE CHANGES');
+        handlePokemonEditClick();
+    }
 
     return (
+        <>
         <h1>Pokemon Edit Page Here</h1>
+
+        <SelectedTeam team={team} handlePokemonEditClick={handlePokemonEditClick}/>
+
+        <div className="button-flex">
+        <Button onClick={handlePokemonEditClick}>Cancel</Button>
+        <Button onClick={saveChanges}>Save</Button>
+        </div>
+        </>
     )
 }
 
