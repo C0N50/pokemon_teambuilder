221bcd44045571c02b4766548d75925d3c43de2e
Connor Ferris <ferr0373@gmail.com>
2023-03-24 01:24:27 -0500

    Started application tutorial page. Completed info page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './AboutPage.css'
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -7,11 +8,29 @@ import React from 'react';
 
 function AboutPage() {
   return (
-    <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
+    <>
+      <h1 className='about-page-title'>How To Use Pokemon Team Builder</h1>
+
+      <div className="about-page-style-container">
+        <div>
+          <p className='paragraph'>Welcome to the Pokemon Team Builder application!
+            The Pokemon Team Builder app is designed to house competitive resources in one comprehensive application
+            to provide a one stop shop for learning,
+            team building, and analytics. This help page is written as a brief tutorial and quickstart guide to the core functions
+            and features of the team builder application.</p>
+
+
+          <p className='paragraph'> To use the Pokemon Team Builder application you first must register with a user name and password.
+          Although there are technically no password requirements, it is advised the the password be of at least 8 characters and contain
+          a mix of Capital & lowercase letters, numbers, and symbols.  
+          </p>
+
+
+        
+
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
