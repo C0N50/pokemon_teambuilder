1fce9a4664ff9933e7bfdeb46f7e0e84ecd80281
Connor Ferris <ferr0373@gmail.com>
2023-03-25 21:09:54 -0500

    added dispatch calls to clear reducers on logout

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -3,13 +3,32 @@ import { useDispatch } from 'react-redux';
 
 function LogOutButton(props) {
   const dispatch = useDispatch();
+
+
+  const handleLogOut = () => {
+
+
+    dispatch ({ type : 'CLEAR_ANALYSIS_TEAM' })
+
+    dispatch ({type : 'CLEAR_SELECTED POKEMON'})
+
+    dispatch ({type : 'DELETE_SELECTED_TEAM'})
+
+    dispatch ({ type : 'CLEAR_SORTED_TEAMS'})
+
+    dispatch ({ type : 'CLEAR_TEAM_LIST'})
+
+    dispatch({ type: 'LOGOUT' })
+
+  }
+
   return (
     <button
       // This button shows up in multiple locations and is styled differently
       // because it's styled differently depending on where it is used, the className
       // is passed to it from it's parents through React props
       className={props.className}
-      onClick={() => dispatch({ type: 'LOGOUT' })}
+      onClick={handleLogOut}
     >
       Log Out
     </button>
